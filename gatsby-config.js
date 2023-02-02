a4fdac9090e631b3e0716da1a5e6fd65971e7f59
SergeyShurekov <shurekov@gmail.com>
2023-02-02 19:31:05 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `HotelAnita`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
